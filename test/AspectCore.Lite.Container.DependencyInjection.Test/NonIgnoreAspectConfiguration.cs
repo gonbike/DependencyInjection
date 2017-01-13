a525163835ae2df99b3ab6bb0e62bc9970c22875
liuhaoyang <liuhaoyang1221@hotmail.com>
2017-01-13 16:14:21 +0800

    Add AspectCore.Lite.Container.DependencyInjection.Test project

---
+++ test/AspectCore.Lite.Container.DependencyInjection.Test/NonIgnoreAspectConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using AspectCore.Lite.Abstractions;
+using System;
+using System.Collections.Concurrent;
+
+namespace AspectCore.Lite.Container.DependencyInjection.Test
+{
+    public class NonIgnoreAspectConfiguration : IAspectConfiguration
+    {
+        private readonly ConcurrentDictionary<Type, object> optionCache;
+
+        public NonIgnoreAspectConfiguration()
+        {
+            optionCache = new ConcurrentDictionary<Type, object>();
+        }
+
+        public IConfigurationOption<TOption> GetConfigurationOption<TOption>()
+        {
+            return (ConfigurationOption<TOption>)optionCache.GetOrAdd(typeof(TOption), key => new ConfigurationOption<TOption>());
+        }
+    }
+}
---
+++ test/AspectCore.Lite.Container.DependencyInjection.Test/SpecificationTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection.Specification;
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using AspectCore.Lite.Abstractions;
+
+namespace AspectCore.Lite.Container.DependencyInjection.Test
+{
+    public class SpecificationTests: DependencyInjectionSpecificationTests
+    {
+        protected override IServiceProvider CreateServiceProvider(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton<IAspectConfiguration, NonIgnoreAspectConfiguration>();
+            var aspectCoreServiceProviderFactory = new AspectCoreServiceProviderFactory();
+            return aspectCoreServiceProviderFactory.CreateServiceProvider(serviceCollection);
+        }
+    }
+}
