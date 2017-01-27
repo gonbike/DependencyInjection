178442710460184d7a30fa2103a14448e229814b
liuhaoyang <liuhaoyang1221@hotmail.com>
2017-01-27 22:01:53 +0800

    update

--- Publish.bat
+++ Publish.bat
@@ -11,10 +11,14 @@ for /R "packages" %%s in (*) do (
 dotnet pack src/AspectCore.Container.Autofac --configuration Release --output packages
 dotnet pack src/AspectCore.Container.DependencyInjection --configuration Release --output packages
 
+for /R "packages" %%s in (*symbols.nupkg) do (
+    del %%s
+)
+
 set /p key=input key:
 set source=https://www.myget.org/F/aspectcore/api/v2/package
 
-for /R "packages" %%s in (*symbols.nupkg) do ( 
+for /R "packages" %%s in (*.nupkg) do ( 
     call nuget push %%s -s %source% %key%
 )
 
