c1285d479bd1752325aa34c9456786f7c04b1abc
liuhaoyang <liuhaoyang1221@hotmail.com>
2017-01-28 21:56:18 +0800

    rename ParameterTargetInstanceProvider to ParameterServiceInstanceProvider

---
+++ src/AspectCore.Container.Autofac/ParameterServiceInstanceProvider.cs
@@ -0,0 +1,20 @@
+﻿using AspectCore.Abstractions.Resolution.Internal;
+using System;
+
+namespace AspectCore.Container.Autofac
+{
+    internal class ParameterServiceInstanceProvider : IServiceInstanceProvider
+    {
+        private readonly object instance;
+
+        public ParameterServiceInstanceProvider(object instance)
+        {
+            this.instance = instance;
+        }
+
+        public object GetInstance(Type serviceType)
+        {
+            return instance;
+        }
+    }
+}
